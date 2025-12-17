0edeb7e8f3077b9a4879e925148ab5fbf62b0fd1
Gabby Elmaliah <gabbyelmaliah@gmail.com>
2025-12-17 03:29:42 +0000

    added slots generator

---
+++ backend/services/slotGenerator.js
@@ -0,0 +1,43 @@
+import { PRIVATE_DURATION, GROUP_DURATION, STEP} from "../config/constatns";
+
+
+const generateSlotsForWindow = (instructor, window, duration, type) => {
+  const slots = [];
+
+  for (
+    let start = window.startMinutes;
+    start + duration <= window.endMinutes;
+    start += STEP
+  ) {
+    slots.push({
+      instructorId: instructor._id.toString(),
+      instructorName: instructor.name,
+      day: window.day,
+      startMinutes: start,
+      endMinutes: start + duration,
+      type
+    });
+  }
+
+  return slots;
+};
+
+
+ const generateSlotsForInstructor = (instructor) => {
+  const slots = [];
+
+  for (const window of instructor.availability) {
+    slots.push(
+      ...generateSlotsForWindow(instructor, window, PRIVATE_DURATION, "private")
+    );
+
+    slots.push(
+      ...generateSlotsForWindow(instructor, window, GROUP_DURATION, "group")
+    );
+  }
+  return slots;
+};
+
+export default generateSlotsForInstructor;
+
+
